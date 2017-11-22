aa037b7a7ee2c9b4b0b7855727e15e13cc8104f2
Ievgen <yevgens@web.de>
2017-11-22 20:50:36 +0200

    add gitignore

--- addons/openacademy/models/models.py
+++ addons/openacademy/models/models.py
@@ -1,15 +1,10 @@
-# -*- coding: utf-8 -*-
+-*- coding: utf-8 -*-
 
 from odoo import models, fields, api
 
-class openacademy(models.Model):
-    _name = 'openacademy.openacademy'
 
-    name = fields.Char()
-    value = fields.Integer()
-    value2 = fields.Float(compute="_value_pc", store=True)
+class Course(models.Model):
+    _name = 'openacademy.course'
+
+    name = fields.Char(string="Title", required=True)
     description = fields.Text()
-
-    @api.depends('value')
-    def _value_pc(self):
-        self.value2 = float(self.value) / 100
