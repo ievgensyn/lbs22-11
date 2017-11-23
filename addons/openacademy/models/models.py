ac9f000706deb0d655827ae844239039e1f8fec7
Ievgen <yevgens@web.de>
2017-11-23 13:33:09 +0200

    Removing files an dir form repo + new indexing

--- addons/openacademy/models/models.py
+++ addons/openacademy/models/models.py
@@ -1,4 +1,4 @@
--*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
 
@@ -8,3 +8,15 @@ class Course(models.Model):
 
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
+
+
+class Session(models.Model):
+    _name = 'openacademy.session'
+
+    name = fields.Char(required=True)
+    start_date = fields.Date()
+    duration = fields.Float(digits=(6, 2), help="Duration in days")
+
+
+
+
